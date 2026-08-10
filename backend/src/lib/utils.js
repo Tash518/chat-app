f4108a36f6451faa1d9df707c5c5e8773950ff28
Tash518 <tashifahmed518@gmail.com>
2026-08-10 21:56:19 +0530

    Dockerize full stack application

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,6 +2,6 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (userID, res) => {
     const token = jwt.sign({id: userID}, process.env.JWT_SECRET, {expiresIn:"7d"});
-    res.cookie("jwt",token,{maxAge:7*24*60*60*1000, httpOnly:true,sameSite:"strict", secure: process.env.NODE_ENV ==="production"})
+    res.cookie("jwt",token,{maxAge:7*24*60*60*1000, httpOnly:true,sameSite:"None", secure: process.env.NODE_ENV ==="production"})
     return token;
 };
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { authReducer, initialState } from './authReducer';
 import { axiosInstance } from '../lib/axios';
 import toast from 'react-hot-toast';
 import { io } from 'socket.io-client';
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = "https://chat-app-4ono.onrender.com/";
 
 //creatin context
 const AuthContext = createContext();
